8f8b588a7355933069a29cc43998f19853575f12
IgorFromUA <zap.ck.ua@gmail.com>
2024-04-27 12:04:00 +0300

    deleted import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 import './styles/normalize.css';
-import './styles/root.css';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
     <React.StrictMode>
